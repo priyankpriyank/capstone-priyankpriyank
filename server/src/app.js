77e900cfbca7b0f96fac588f8d50fc6794c1ced6
priyankpriyank <vorapriyankp@gmail.com>
2026-09-14 11:19:05 -0400

    updated the npm script and tested the API

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 // simple route to check if the server is running
 app.get("/api/health", (req, res) => {
   res.json({
@@ -15,5 +16,6 @@ app.get("/api/health", (req, res) => {
     message: "NexTo API is running"
   });
 });
+
 
 module.exports = app;
