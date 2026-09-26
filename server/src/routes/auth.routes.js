423149089d53ed23971d524261a6f38ee31e3745
priyankpriyank <vorapriyankp@gmail.com>
2026-09-26 12:56:28 -0400

    Add reset password route to authentication API

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -3,7 +3,8 @@ const {
   register,
   login,
   logout,
-  forgotPassword
+  forgotPassword,
+  resetPassword
 } = require("../controllers/auth.controller");
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
